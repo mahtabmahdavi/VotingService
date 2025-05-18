252f7484b0345cd129e6786a1122daecc8777858
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2025-05-18 21:03:25 +0330

    Implement getting all polls method

--- projects/VotingService/Grpc/Services/VoteGrpcService.cs
+++ projects/VotingService/Grpc/Services/VoteGrpcService.cs
@@ -1,12 +1,28 @@
 ﻿using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using VotingService.Application.Intefaces.Repositories;
+using VotingService.Application.Mappers;
 using VotingService.Protos;
 
 namespace VotingService.Grpc.Services;
 
 public class VoteGrpcService : VoteService.VoteServiceBase
 {
-    
+    private readonly IPollRepository _repository;
+    private readonly ILogger<VoteGrpcService> _logger;
+
+    public VoteGrpcService(IPollRepository repository, ILogger<VoteGrpcService> logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+
+    public override async Task<PollListResponse> GetPolls(Protos.Empty request, ServerCallContext context)
+    {
+        var polls = await _repository.GetAllPollsAsync();
+        var response = new PollListResponse();
+        response.Polls.AddRange(polls.Select(p => p.ToProto()));
+        return response;
+    }
 }
 
