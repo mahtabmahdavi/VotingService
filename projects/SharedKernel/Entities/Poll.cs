57bebe26a0460ae0138ca7c639d84905edc875d8
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2025-05-18 11:44:54 +0330

    Add domain entities (Poll, Question, Option, Vote)

---
+++ projects/SharedKernel/Entities/Poll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.Entities;
+
+public class Poll
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<Question> Questions { get; set; } = new List<Question>();
+}
