e461c9c0691a87ced4859ca31fb8f8d203ec294a
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2025-05-18 18:54:43 +0330

    Implement vote repository

---
+++ projects/VotingService/Repositories/Interfaces/IVoteRepository.cs
@@ -0,0 +1,10 @@
+﻿using SharedKernel.Entities;
+
+namespace VotingService.Repositories.Interfaces;
+
+public interface IVoteRepository
+{
+    Task<IEnumerable<Poll>> GetAllPollsAsync();
+    Task<Poll?> GetPollByIdAsync(Guid id);
+    Task<bool> SubmitVoteAsync(Guid optionId, string voterId);
+}
---
+++ projects/VotingService/Repositories/VoteRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedKernel.Entities;
+using VotingService.Persistence.Context;
+using VotingService.Repositories.Interfaces;
+
+namespace VotingService.Repositories;
+
+public class VoteRepository : IVoteRepository
+{
+    private readonly VotingDbContext _context;
+
+    public VoteRepository(VotingDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Poll>> GetAllPollsAsync()
+    {
+        return await _context.Polls
+            .Include(p => p.Questions)
+                .ThenInclude(q => q.Options)
+            .ToListAsync();
+    }
+
+    public async Task<Poll?> GetPollByIdAsync(Guid id)
+    {
+        return await _context.Polls
+            .Include(p => p.Questions)
+                .ThenInclude(q => q.Options)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<bool> SubmitVoteAsync(Guid optionId, string voterId)
+    {
+        var optionExists = await _context.Options.AnyAsync(o => o.Id == optionId);
+        if (!optionExists) return false;
+
+        var vote = new Vote
+        {
+            Id = Guid.NewGuid(),
+            OptionId = optionId,
+            VoterId = voterId,
+            VotedAt = DateTime.UtcNow
+        };
+
+        _context.Votes.Add(vote);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
